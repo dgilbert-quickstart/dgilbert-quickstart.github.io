5d6891f994c5a94f007a81e5b177d2cf80705e1f
dgilbert-quickstart <dgilbert.quickstart@gmail.com>
2024-07-09 20:27:11 +0100

    demo week4 website update

--- demo-week4-website-1/index.js
+++ demo-week4-website-1/index.js
@@ -2,15 +2,22 @@
 
 //index.js 
 
+/*
+
+multi line javascript 
+
+*/
 function ex1() {
 
   //DOM intro - document object model 
-  const output1 = document.getElementById('output1');
+  //es5+
+  const output1 = document.getElementById("output1")
 
   //es1..4
-  console.log("* ex1 js into");
+  console.log(' ex1 js into');
+  
   //es5+
-  console.log(`* ex1 js into`);
+  console.log(`* ex1 js into `)
   
   //dynamic js development - updating html/ui without refreshing the browser: ie: SPA - React 
 
@@ -39,8 +46,8 @@ function ex2() {
 
 }
 
-
 function ex3() {
+
   const output1 = document.getElementById('output1');
 
   //es1..4
@@ -54,7 +61,34 @@ function ex3() {
   //es5+
   output1.innerText = `ex3 es5 intro to js`
 
+  //return nothing or void 
   return
-  //return true;
-  //return false;
+
+  //return true; //allow click event to continue
+  //return false; //stops the clieck event 
+  //reutrn any valid javascipt data type 
+
+}
+
+function display_date_time1(){
+  
+  //using document DOM, select the date element - lbldate
+  const eledate = document.getElementById('lbldate');
+
+  //using document DOM, select the time element - lbltime
+
+  //create a date variable 
+  
+  //new - creates a new copy or instance in memory 
+  const d1 = new Date();  //combines date and time
+
+  //create a time variable 
+
+  //update lbldate.innerText
+  let _month = (d1.getMonth() + 1);  //math order of opertion (), *, +
+
+  eledate.innerText = _month + "/" + d1.getDate() + "/" + d1.getFullYear();
+
+  //update lbltime.innerText 
+
 }
