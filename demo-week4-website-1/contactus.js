7b1c7601c8e7a60b4b4d5fb5b43b0898f4bfcec8
dgilbert-quickstart <dgilbert.quickstart@gmail.com>
2024-07-09 22:31:39 +0100

    update contactus page

---
+++ demo-week4-website-1/contactus.js
@@ -0,0 +1,20 @@
+/*
+
+validation reuirement
+----------------------------------
+
+- name 
+    required fields not empty 
+    text field max 30 chacters 
+
+- phone optional 
+
+- email 
+    required fields not empty 
+    text field max 100 chacters 
+
+- comment 
+    required fields not empty 
+    text field max 150 chacters 
+
+*/
