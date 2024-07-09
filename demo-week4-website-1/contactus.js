6b15a011034b673ebdbb95f659d92fe27b8574f1
dgilbert-quickstart <dgilbert.quickstart@gmail.com>
2024-07-09 23:27:39 +0100

    update contactus javascript form validation

--- demo-week4-website-1/contactus.js
+++ demo-week4-website-1/contactus.js
@@ -18,3 +18,37 @@ validation reuirement
     text field max 150 chacters 
 
 */
+
+//es1..4
+//refactor es5+
+
+function vlaidate_form()
+{
+
+    //es1..4 -- updatable variable 
+    var elename = document.getElementById("txtname");
+
+    //es5+ // updatable variable 
+    let elephone = document.getElementById("txtphone");
+
+    //es5+ // readonly variable 
+    const eleemail = document.getElementById("txtemail");
+    
+    //es5+ // readonly variable 
+    const elecomment = document.getElementById("txtcomment");
+
+    if(elename.value == "") //evaluate left and right statement
+    {
+        //if true
+        alert("* please enter a valid name");
+
+        //cancel form submit 
+        return false;    
+    }
+
+    //cancel form submit 
+    //return false;
+
+    //continue form submit 
+    return true;
+}
