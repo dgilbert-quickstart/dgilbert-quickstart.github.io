3573b5274b6f364b02031272c406c3c5cd5333ea
dgilbert-quickstart <dgilbert.quickstart@gmail.com>
2024-07-16 17:49:52 +0100

    update shopping cart page, add arry, object

--- demo-week4-website-1/js/shoppingcart.js
+++ demo-week4-website-1/js/shoppingcart.js
@@ -63,9 +63,59 @@ document.getElementById("btnaddnew").addEventListener("click", function(e)
 {
     e.preventDefault();
     const output1 = document.getElementById("output1");
+    const addnewmessage = document.getElementById("addnewitem-message");
+    const txtaddname = document.getElementById("txtname-new");
+    const txtaddprice = document.getElementById("txtprice-new");
+    
+    //-- javascript local scope, global scope 
+    //-- Application Refactor
+    //-- application global data store /state/ ie: cookies, localstorage, sessionstorage, indexdb, react/redux 
+    //-- BrowserDev Tools to provew application date/state 
+    // create shopping cart array 
+    let _shoppingcart = [];
+
+    //validate only add max 10 items 
+
+    if(txtaddname.value === "")
+    {
+        const _msg = "* please enter a name";
+        addnewmessage.innerText = _msg;
+        return false;
+    }
+    else if(txtaddprice.value === "")
+    {
+        const _msg = "* please enter a price";
+        addnewmessage.innerText = _msg;
+        return false;
+    }
+
+    //convert string to number - parseFloat()
+    const _price = parseFloat(txtaddprice.value);
+    //format number to fixed to 2 decimal places, _price.toFixed(2)
+    //validate number isNan() - ie: abc, 1ab
+    //error handling 
+
+    //create a new shoppingcart entry/object ie: {name, price}
+    //javascript object = json document, NOSQL ie: mongodb and other systems 
+    var _cartitem = {name:txtaddname, price:_price};
+
+    //add shoppingcart entry/object to ul/list
+    _shoppingcart.push(_cartitem);
+
+    let _total = 0.0;
+    //update ui/li with cart item 
+    for(let i=0;i<_shoppingcart.length;i++)
+    {
+        //use Dom.CreateElement, appendChild 
+        output1.innerHTML = output1.innerHTML + _shoppingcart[i] + "<br/>";
+        _total = _total + _price;
+    }
+
+    //update total field 
+
     _msg = "btnaddnew"
     console.log(_msg)
-    output1.innerText = _msg;
+    //output1.innerText = _msg;
 });
 
 document.getElementById("btnclear-addnew").addEventListener("click", function(e)
