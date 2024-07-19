81c8f6691225f79ed66d9a958be2869c6d1abd0f
dgilbert-quickstart <dgilbert.quickstart@gmail.com>
2024-07-19 22:36:40 +0100

    create demo responsive api browser app 2.0

---
+++ Demo-week4-api-browser-app-2/js/index.js
@@ -0,0 +1,9 @@
+function closenavmenu() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+
