44506be5c625c0e25e6f7c4b4cc4942fc13e7680
dgilbert-quickstart <dgilbert-quickstart@gmail.com>
2024-07-31 21:40:55 +0100

    update demo week 7 website demo update: js script include

---
+++ demo-week7-website-1/js/page1-lib1.js
@@ -0,0 +1,21 @@
+//js global scope 
+//window - global scope 
+// also called hoisting 
+
+//-- es1-4+
+
+//-- code reuse----
+
+//-- fractor: 
+// log parameter: console, file, db, etc..
+// default parameters
+// log class
+// functional programming - logfunction
+// class/function - namespaces - same name errors
+// error handling 
+// other 
+
+function log(massage){
+    console.log(massage)
+}
+
