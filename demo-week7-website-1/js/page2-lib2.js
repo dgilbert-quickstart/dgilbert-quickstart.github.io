5274ec775c02b5cf92da7c611efe20a56fcbe784
dgilbert-quickstart <dgilbert-quickstart@gmail.com>
2024-07-31 23:48:11 +0100

    update demo week 7 website demo update: es5+ js es module

---
+++ demo-week7-website-1/js/page2-lib2.js
@@ -0,0 +1,20 @@
+//-- es5+
+
+//-- code reuse----
+
+//-- fractor: 
+// log parameter: console, file, db, etc..
+// default parameters
+// log class
+// functional programming - logfunction
+// class/function - namespaces - same name errors
+// error handling 
+// other 
+
+export function calc_add(x, y){
+    return (x+y)
+}
+
+export function calc_multiply(x, y){
+    return (x*y)
+}
