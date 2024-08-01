a4a31a55fcddec80a1375dac63ed0a67ace6001c
dgilbert-quickstart <dgilbert-quickstart@gmail.com>
2024-08-01 19:08:51 +0100

    update demo week 7 website demo - update page3 oop es1-4, es5

---
+++ demo-week7-website-1/js/page3.js
@@ -0,0 +1,45 @@
+
+document.getElementById("btn-oop-obj-ex1").addEventListener("click", (e)=>{
+    
+    const _function_name = "page3::btn-oop-obj-ex1";
+    const _output1 = document.getElementById("output1");
+
+    try {        
+
+        _output1.innerText = _function_name;
+
+    } catch (error) {
+        
+        _output1.innerText = `${_function_name}::error - ${error}`;
+    }
+});
+
+document.getElementById("btn-oop-obj-func-ex1").addEventListener("click", (e)=>{
+    
+    const _function_name = "page3::btn-oop-obj-func-ex1";
+    const _output1 = document.getElementById("output1");
+
+    try {        
+
+        _output1.innerText = _function_name;
+
+    } catch (error) {
+        
+        _output1.innerText = `${_function_name}::error - ${error}`;
+    }
+});
+
+document.getElementById("btn-oop-es5-class-ex1").addEventListener("click", (e)=>{
+    
+    const _function_name = "page3::btn-oop-es5-class-ex1";
+    const _output1 = document.getElementById("output1");
+
+    try {        
+
+        _output1.innerText = _function_name;
+
+    } catch (error) {
+        
+        _output1.innerText = `${_function_name}::error - ${error}`;
+    }
+});
