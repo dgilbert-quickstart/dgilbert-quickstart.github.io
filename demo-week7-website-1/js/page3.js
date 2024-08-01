9a69243898d062d12db5fcbc1961b355bf8365ae
dgilbert-quickstart <dgilbert-quickstart@gmail.com>
2024-08-01 22:01:51 +0100

    update demo week 7 website demo - update page3 oop es1-4

--- demo-week7-website-1/js/page3.js
+++ demo-week7-website-1/js/page3.js
@@ -14,6 +14,21 @@ document.getElementById("btn-oop-obj-ex1").addEventListener("click", (e)=>{
     }
 });
 
+document.getElementById("btn-oop-obj-ex2").addEventListener("click", (e)=>{
+    
+    const _function_name = "page3::btn-oop-obj-ex2";
+    const _output1 = document.getElementById("output1");
+
+    try {        
+
+        _output1.innerText = oop_obj_es4_ex2();
+
+    } catch (error) {
+        
+        _output1.innerText = `${_function_name}::error - ${error}`;
+    }
+});
+
 document.getElementById("btn-oop-obj-func-ex1").addEventListener("click", (e)=>{
     
     const _function_name = "page3::btn-oop-obj-func-ex1";
