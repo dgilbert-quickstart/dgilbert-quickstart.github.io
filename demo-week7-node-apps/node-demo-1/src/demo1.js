e250f4f6e4eb0e940a0b27bd53fd06d9ce89eafa
dgilbert-quickstart <dgilbert-quickstart@gmail.com>
2024-08-02 19:09:19 +0100

    create demo node backend app-1

---
+++ demo-week7-node-apps/node-demo-1/src/demo1.js
@@ -0,0 +1,28 @@
+//
+//ie: backend nodejs web server or web application 
+//
+
+/*
+refactor: 
+- add package.json
+  npm init -y
+- install packages
+  npm i uuid 
+  or
+  npm i 
+  ** if previously installed package 
+- add .gitignore 
+*/
+
+//es1-6+
+function main()
+{
+    const _msg = `intro to node 20 - 1.0.0`;
+    console.log(_msg);
+}
+
+
+main();
+
+//node demo1.js
+
